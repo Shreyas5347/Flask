a8f490374053b9494daa23c0a9da55f07e6a1e12
Shreyas5347 <rajputshreyas321@gmail.com>
2026-02-02 19:41:17 +0530

    about the code

--- flask1.py
+++ flask1.py
@@ -1,5 +1,42 @@
+'''
+Flask is request-driven: each route executes only when its URL is requested.
+Concept	                   |   Analogy
+Flask                	   |   Blueprint
+app = Flask(...)	       |   Actual house
+app.run()	               |    Open the house to visitors
+
+
+'''
 from flask  import Flask,request,jsonify
 app=Flask(__name__)
 @app.route("/")
 def home():
     return "hello world via flask"
+'''
+home = app.route("/")(home)
+So @app.route("/") means:
+
+“Pass the function home into app.route("/") and replace it with the result.”
+@ is a decorator - A decorator in Python is:
+Decorator = wrapper Just like wrapping a gift 🎁 Decorator wraps a function
+'''
+
+'''
+app = Flask(__name__)
+Flask is a class.
+app is an instance of that class.
+it says:
+“Create a web application object.”
+
+'''
+'''
+If you run a file directly:
+python flask1.py
+Then inside flask1.py:
+__name__ == "__main__"
+
+If the file is imported then – 
+import flask1
+Then inside flask1.py:
+__name__ == "flask1"
+'''
