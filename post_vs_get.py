5905564edcae72aa7bff2449e4ecb24ce52d4ec3
Shreyas5347 <rajputshreyas321@gmail.com>
2026-02-04 22:33:13 +0530

    post vs get

--- post_vs_get.py
+++ post_vs_get.py
@@ -1,7 +1,9 @@
+#login using post method
+#GET = ask for data
+#POST = send data
+
 from flask import Flask, request, jsonify
-
 app = Flask(__name__)
-
 @app.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
@@ -19,4 +21,8 @@ def login():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+#we use get to avoid username and password leak in url or browser history 
+#post is used to send data to server So → POST 
+#post is more secure than get
 
