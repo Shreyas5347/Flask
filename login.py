96768fea9f353cb0de22af6b56cbb3ac18fff10e
Shreyas5347 <rajputshreyas321@gmail.com>
2026-02-08 22:00:27 +0530

    fetching hashed password and checking whether its correct or not

--- login.py
+++ login.py
@@ -1,5 +1,5 @@
 from flask import Flask,request,jsonify
-
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 app=Flask(__name__)
 
@@ -15,13 +15,16 @@ def login():
     
     conn=sqlite3.connect("test.db")
     cursor=conn.cursor()
-    
-    cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (user['username'], user['password']))
+    cursor.execute("SELECT password FROM users WHERE username = ?",
+        (username,))
     result=cursor.fetchone()
-    print(result)
-    conn.close()
-    if result:
+    if not result:
+        return jsonify({"error":"User not found"}),404
+    
+    if check_password_hash(result[0], password):
         return jsonify({"message":"Login successful"})
     else:
         return jsonify({"error":"Invalid credentials"})
+    
+    conn.close()
 
