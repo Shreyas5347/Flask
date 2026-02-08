8a8ae3060ddecd9939a6fe49d2008e23e40a0beb
Shreyas5347 <rajputshreyas321@gmail.com>
2026-02-08 12:24:28 +0530

    basic login code

---
+++ login.py
@@ -0,0 +1,27 @@
+from flask import Flask,request,jsonify
+
+import sqlite3
+app=Flask(__name__)
+
+@app.route("/login", methods=["POST"])
+def login():
+    user=request.get_json()
+    if not user:
+        return jsonify({"error":"No data provided"}),400
+    username=user.get("username")
+    password=user.get("password")
+    if not username or not password:
+        return jsonify({"error":"Username and password are required"}),400
+    
+    conn=sqlite3.connect("test.db")
+    cursor=conn.cursor()
+    
+    cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (user['username'], user['password']))
+    result=cursor.fetchone()
+    print(result)
+    conn.close()
+    if result:
+        return jsonify({"message":"Login successful"})
+    else:
+        return jsonify({"error":"Invalid credentials"})
+
